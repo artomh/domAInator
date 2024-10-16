108fe137c09d06591884140de55e454eeee82b2c
artomh <artom.h@hotmail.com>
2024-10-16 12:08:43 -0400

    set PORT to run on local and azure web app

--- index.js
+++ index.js
@@ -66,7 +66,6 @@ app.post("/domaingen", async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
